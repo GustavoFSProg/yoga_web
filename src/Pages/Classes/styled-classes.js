37497a6c4df39e74daa47e3fb8041fb65032f171
Gustavo <gustavo.prog40@gmail.com>
2022-04-03 15:29:34 -0300

    refactor 22

--- src/Pages/Classes/styled-classes.js
+++ src/Pages/Classes/styled-classes.js
@@ -15,7 +15,7 @@ export const CardContainer = styled.div`
   @media screen and (max-width: 800px) {
     display: flex;
     flex-direction: column;
-    margin-top: 485px;
+    margin-top: 472px;
     align-items: center;
     justify-content: center;
     margin-left: 20px;
